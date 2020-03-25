9600d9b699fac328db491632a1d2f7c15ac281fc
Misha Marinenko <marinenko.m@d2.digital>
2020-03-25 21:00:41 +0200

    feat(notifications)

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import iQOS from "iqos";
+const Notification = window.Notification;
+
 const update = () => {
   store.state.batteryValue = iqos.batteryValue;
 };
@@ -10,7 +12,8 @@ const iqos = new iQOS(navigator.bluetooth, update);
 Vue.use(Vuex);
 export const store = new Vuex.Store({
   state: {
-    batteryValue: null
+    batteryValue: null,
+    notificationHolderReadyShown: false
   },
   mutations: {
     start() {
@@ -23,6 +26,20 @@ export const store = new Vuex.Store({
   },
   actions: {},
   modules: {}
+});
+
+store.watch(state => {
+  if (state.batteryValue.holderReady) {
+    if (Notification.permission === "granted") {
+      new Notification("Holder Ready ✅");
+    } else if (Notification.permission !== "denied") {
+      Notification.requestPermission(permission => {
+        if (permission === "granted") {
+          new Notification("Holder Ready ✅");
+        }
+      });
+    }
+  }
 });
 
 export default store;
